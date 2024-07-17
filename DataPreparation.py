3084d26b360f35ff2ae405fefdf52f535aed3cc4
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2024-07-17 21:08:14 +0530

    Create DataPreparation.py

---
+++ DataPreparation.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report,confusion_matrix
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+df = pd.read_csv('YOUR_CSV_FILE_LOCATIONS')
+
+# Preprocessing function
+def preprocess_text(text):
+    # Lowercasing
+    text = text.lower()
+    # Tokenization
+    tokens = word_tokenize(text)
+    # Removing stop words
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    # Join tokens back to string
+    return ' '.join(tokens)
+
+# Apply preprocessing
+df['Description'] = df['Description'].apply(preprocess_text)
